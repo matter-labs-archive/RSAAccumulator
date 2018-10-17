3e549557378fa9400b0f32727baa4c48da9b8450
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-10-17 11:33:28 +0300

    initial commit

---
+++ contracts/RSAAccumulator.sol
@@ -0,0 +1,303 @@
+pragma solidity ^0.4.25;
+
+contract RSAAccumulator {
+    // all arithmetics is modulo N
+
+    uint256 constant public NlengthIn32ByteLimbs = 8; // 1024 bits for factors, 2048 for modulus
+    uint256 constant public NlengthInBytes = 32 * NlengthIn32ByteLimbs;
+    uint256 constant public NLength = NlengthIn32ByteLimbs * 8 * 32;
+    uint256 constant public g = 3;
+
+    uint256[NlengthIn32ByteLimbs] public emptyAccumulator;
+    uint256[NlengthIn32ByteLimbs] public accumulator; // try to store as static array for now; In BE
+    uint256[NlengthIn32ByteLimbs] public N;
+
+    event AccumulatorUpdated(uint256 indexed _coinID);
+    event DebugEvent(uint256 _uint, bool _bool);
+    // constructor(uint256[NlengthIn32ByteLimbs] modulus) public {
+    //     accumulator[NlengthIn32ByteLimbs - 1] = g;
+    //     N = modulus;
+    // }
+
+    constructor(bytes modulus) public {
+        accumulator[NlengthIn32ByteLimbs - 1] = g;
+        emptyAccumulator[NlengthIn32ByteLimbs - 1] = g;
+        require(modulus.length == NlengthInBytes, "Modulus should be at least padded");
+        uint256 limb = 0;
+        uint256 dataLength = 0x20; // skip length;
+        for (uint256 i = 0; i < NlengthIn32ByteLimbs; i++) {
+            assembly {
+                limb := mload(add(modulus, dataLength))        
+            }
+            N[i] = limb;
+            dataLength += 0x20;
+        }
+    }
+
+    function updateAccumulator(uint256 _value) public {
+        accumulator = modularExp(accumulator, _value, N);
+    }
+
+    function includeCoin(uint256 _coinID) public {
+        accumulator = modularExp(accumulator, mapCoinToPrime(_coinID), N);
+        emit AccumulatorUpdated(_coinID);
+    }
+
+    function mapCoinToPrime(uint256 _id) public
+    view
+    returns (uint256 prime) {
+        // Sony's way to determine randomness :)
+        return 11;
+    }
+
+    function getN()
+    public
+    view
+    returns (uint256[NlengthIn32ByteLimbs] n) {
+        return N;
+    }
+
+    function mapHashToPrime(bytes32 _hash) public
+    view
+    returns (uint256 prime) {
+        // Another Sony's way to determine randomness :)
+        return 17;
+    }
+
+    function calculateProof(uint256 _coinID, uint256 x)
+    public 
+    view 
+    returns (uint256[NlengthIn32ByteLimbs] b, uint256[NlengthIn32ByteLimbs] z, uint256 r) {
+        uint256[NlengthIn32ByteLimbs] memory nReadOnce = N;
+        uint256[NlengthIn32ByteLimbs] memory h = modularExp(emptyAccumulator, mapCoinToPrime(_coinID), nReadOnce);
+        z = modularExp(h, x, nReadOnce);
+        uint256 B = mapHashToPrime(keccak256(abi.encodePacked(h, z)));
+        uint256 exp = x / B;
+        b = modularExp(h, exp, nReadOnce);
+        r = x % B;
+    }
+
+    function checkProof(
+        uint256 _coinID,
+        uint256[NlengthIn32ByteLimbs] b, 
+        uint256[NlengthIn32ByteLimbs] z, 
+        uint256 r)
+    public 
+    // view 
+    returns (bool isValid) {
+        uint256[NlengthIn32ByteLimbs] memory nReadOnce = N;
+        uint256[NlengthIn32ByteLimbs] memory h = modularExp(emptyAccumulator, mapCoinToPrime(_coinID), nReadOnce);
+        uint256 B = mapHashToPrime(keccak256(abi.encodePacked(h, z))); // no mod N due to size difference
+        uint256[NlengthIn32ByteLimbs] memory b_B = modularExp(b, B, nReadOnce);
+        uint256[NlengthIn32ByteLimbs] memory h_R = modularExp(h, r, nReadOnce);
+        uint256[NlengthIn32ByteLimbs] memory lhs = mulMod(b_B, h_R, nReadOnce);
+        for (uint256 i = 0; i < NlengthIn32ByteLimbs; i++) {
+            if (lhs[i] != z[i]) {
+                emit DebugEvent(lhs[i], false);
+                emit DebugEvent(z[i], true);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function mulMod(
+        uint256[NlengthIn32ByteLimbs] _a,
+        uint256[NlengthIn32ByteLimbs] _b,
+        uint256[NlengthIn32ByteLimbs] _m)
+    public 
+    view 
+    returns (uint256[NlengthIn32ByteLimbs] c) {
+        uint256[NlengthIn32ByteLimbs] memory aPlusB = modularExp(modularAdd(_a, _b, _m), 2, _m);
+        uint256[NlengthIn32ByteLimbs] memory aMinusB = modularExp(modularSub(_a, _b, _m), 2, _m);
+        uint256[NlengthIn32ByteLimbs] memory t = modularSub(aPlusB, aMinusB, _m);
+        // divide by 4
+        for (uint256 i = 0; i < NlengthIn32ByteLimbs - 1; i++) {
+            c[i] = (t[i] >> 2) | (t[i+1] << 254);
+        }
+        c[NlengthIn32ByteLimbs - 1] = t[NlengthIn32ByteLimbs - 1] >> 2;
+        return c;
+    }
+
+    function compare(
+        uint256[NlengthIn32ByteLimbs] _a, 
+        uint256[NlengthIn32ByteLimbs] _b)
+    public
+    view
+    returns (int256 result) {
+        for (uint256 i = 0; i < NlengthIn32ByteLimbs; i++) {
+            if (_a[i] > _b[i]) {
+                return 1;
+            } else if (_a[i] < _b[i]) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    function wrappingSub(
+        uint256[NlengthIn32ByteLimbs] _a, 
+        uint256[NlengthIn32ByteLimbs] _b
+    )
+    public
+    view 
+    returns (uint256[NlengthIn32ByteLimbs] o) {
+        bool borrow = false;
+        uint256 limb = 0;
+        for (uint256 i = NlengthIn32ByteLimbs - 1; i < NlengthIn32ByteLimbs; i--) {
+            limb = _a[i];
+            if (borrow) {
+                if (limb == 0) {
+                    borrow = true;
+                    limb--;
+                    o[i] = limb - _b[i];
+                } else {
+                    limb--;
+                    if (limb >= _b[i]) {
+                        borrow = false;
+                    }
+                    o[i] = limb - _b[i];
+                }
+            } else {
+                if (limb < _b[i]) {
+                    borrow = true;
+                }
+                o[i] = limb - _b[i];
+            }
+        }
+        return o;
+    }
+
+    function wrappingAdd(
+        uint256[NlengthIn32ByteLimbs] _a, 
+        uint256[NlengthIn32ByteLimbs] _b
+    )
+    public
+    view 
+    returns (uint256[NlengthIn32ByteLimbs] o) {
+        bool carry = false;
+        uint256 limb = 0;
+        uint256 subaddition = 0;
+        for (uint256 i = NlengthIn32ByteLimbs - 1; i < NlengthIn32ByteLimbs; i--) {
+            limb = _a[i];
+            if (carry) {
+                if (limb == uint256(~0)) {
+                    carry = true;
+                    o[i] = _b[i];
+                } else {
+                    limb++;
+                    subaddition = limb + _b[i];
+                    if (subaddition >= limb) {
+                        carry = false;
+                    }
+                    o[i] = subaddition;
+                }
+            } else {
+                subaddition = limb + _b[i];
+                if (subaddition < limb) {
+                    carry = true;
+                }
+                o[i] = subaddition;
+            }
+        }
+        return o;
+    }
+
+    function modularSub(
+        uint256[NlengthIn32ByteLimbs] _a, 
+        uint256[NlengthIn32ByteLimbs] _b,
+        uint256[NlengthIn32ByteLimbs] _m)
+    public
+    view 
+    returns (uint256[NlengthIn32ByteLimbs] o) {
+        int256 comparison = compare(_a, _b);
+        if (comparison == 0) {
+            return o;
+        } else if (comparison == 1) {
+            return wrappingSub(_a, _b);
+        } else {
+            uint256[NlengthIn32ByteLimbs] memory space = wrappingSub(_m, _a);
+            return wrappingAdd(_b, space);
+        }
+    }
+
+    function modularAdd(
+        uint256[NlengthIn32ByteLimbs] _a, 
+        uint256[NlengthIn32ByteLimbs] _b,
+        uint256[NlengthIn32ByteLimbs] _m)
+    public
+    view 
+    returns (uint256[NlengthIn32ByteLimbs] o) {
+        uint256[NlengthIn32ByteLimbs] memory space = wrappingSub(_m, _a);
+        // see how much "space" has left before an overflow
+        int256 comparison = compare(space, _b);
+        if (comparison == 0) {
+            return o;
+        } else if (comparison == 1) {
+            return wrappingAdd(_a, _b);
+        } else {
+            return wrappingSub(_b, space);
+        }
+    }
+
+    // this assumes that exponent in never larger than 256 bits
+    function modularExp(
+        uint256[NlengthIn32ByteLimbs] base, 
+        uint256 e, 
+        uint256[NlengthIn32ByteLimbs] m) 
+    public view returns (uint256[NlengthIn32ByteLimbs] output) {
+        uint256 modulusLength = NlengthInBytes;
+        uint256 memoryPointer = 0;
+        uint256 dataLength = 0;
+        assembly {
+            // define pointer
+            memoryPointer := mload(0x40)
+            // store data assembly-favouring ways
+            mstore(memoryPointer, modulusLength)    // Length of Base
+            mstore(add(memoryPointer, 0x20), 0x20)  // Length of Exponent
+            mstore(add(memoryPointer, 0x40), modulusLength)  // Length of Modulus
+        }
+        dataLength = 0x60;
+        // now properly pack bases, etc
+        uint256 limb = 0;
+        for (uint256 i = 0; i < NlengthIn32ByteLimbs; i++) {
+            limb = base[i];
+            assembly {
+                mstore(add(memoryPointer, dataLength), limb)  // cycle over base
+            }
+            dataLength += 0x20;
+        }
+
+        assembly {
+            mstore(add(memoryPointer, dataLength), e)     // Put exponent
+        }
+        dataLength += 0x20;
+
+        for (i = 0; i < NlengthIn32ByteLimbs; i++) {
+            limb = m[i];
+            assembly {
+                mstore(add(memoryPointer, dataLength), limb)  // cycle over base
+            }
+            dataLength += 0x20;
+        }
+        // do the call
+        assembly {
+            let success := staticcall(sub(gas, 2000), 0x05, memoryPointer, dataLength, memoryPointer, modulusLength) // here we overwrite!
+            // gas fiddling
+            switch success case 0 {
+                revert(0, 0)
+            }
+        }
+        dataLength = 0;
+        limb = 0;
+        for (i = 0; i < NlengthIn32ByteLimbs; i++) {
+            assembly {
+                limb := mload(add(memoryPointer, dataLength))
+            }
+            dataLength += 0x20;
+            output[i] = limb;
+        }
+        return output;
+    }
+
+}
---
+++ test/test.js
@@ -0,0 +1,203 @@
+const RSAAccumulatorArtifact = artifacts.require('RSAAccumulator');
+const crypto = require("crypto");
+
+contract('RSA accumulator', async (accounts) => {
+    const BN = require("bn.js");
+
+    const account = accounts[0];
+    let contract;
+    
+    beforeEach(async () => {
+        const modulusHex = "b2f5fd3f9f0917112ce42f8bf87ed676e15258be443f36deafb0b69bde2496b495eaad1b01cad84271b014e96f79386c636d348516da74a68a8c70fba882870c47b4218d8f49186ddf72727b9d80c21911c3e337c6e407ffb47c2f2767b0d164d8a1e9af95f6481bf8d9edfb2e3904b2529268c460256fafd0a677d29898f10b1d15128a695839fc08edd584e8335615b1d1d7277be65c532dca92ddc7050374868b117ea9154914ef9292b8443f13696e4fad50ded6bd90e5a6f7ed33be2ece31c6dd7a4253ee6cdc56787ddd1d5cd776614022db87d03bb22f23285b5a3167af8dacabbea40004471337d3781e8c5cca0ea5e27799b510e4ef938c61caa60d"
+        // modulus us exactly 2048 bits
+        contract = await RSAAccumulatorArtifact.new("0x" + modulusHex, {from: account});
+    })
+
+    it('test overflowing addition', async () => {
+        for (let j = 0; j < 10; j++) {
+            const NlengthIn32ByteLimbs = await contract.NlengthIn32ByteLimbs();
+            const modulusLength = NlengthIn32ByteLimbs.toNumber() * 32
+            const a = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const b = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const modulusLimbs = await contract.getN();
+            const modulus = accumulatorToBN(modulusLimbs, NlengthIn32ByteLimbs.toNumber());
+
+            const aString = a.toString(16);
+
+            const wrapperingModulus = (new BN(1)).ushln(modulusLength * 8);
+            const result = a.add(b).umod(wrapperingModulus);
+
+            const aLimbs = bnToAccumulator(a, NlengthIn32ByteLimbs.toNumber())
+            let aLimbsString = aLimbs[0].toString(16)
+            for (let i = 1; i < NlengthIn32ByteLimbs.toNumber(); i++) {
+                aLimbsString = aLimbsString + aLimbs[i].toString(16).padStart(64, "0");
+            }
+            assert(aString === aLimbsString, "Invalid conversion to limbs");
+
+            const bLimbs = bnToAccumulator(b, NlengthIn32ByteLimbs.toNumber())
+            const contractResultLimbs = await contract.wrappingAdd(aLimbs, bLimbs);
+
+            const resultString = result.toString(16);
+            let resultLimbsString = contractResultLimbs[0].toString(16)
+            for (let i = 1; i < NlengthIn32ByteLimbs.toNumber(); i++) {
+                resultLimbsString = resultLimbsString + contractResultLimbs[i].toString(16).padStart(64, "0");
+            }
+            assert(resultString === resultLimbsString, "Invalid conversion to limbs");
+
+            const contractResult = accumulatorToBN(contractResultLimbs, NlengthIn32ByteLimbs.toNumber());
+
+            assert(contractResult.eq(result), "Addition results are different");
+        }
+    })
+
+    it('test overflowing subtraction', async () => {
+        for (let j = 0; j < 10; j++) {
+            const NlengthIn32ByteLimbs = await contract.NlengthIn32ByteLimbs();
+            const modulusLength = NlengthIn32ByteLimbs.toNumber() * 32
+            const a = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const b = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const modulusLimbs = await contract.getN();
+            const modulus = accumulatorToBN(modulusLimbs, NlengthIn32ByteLimbs.toNumber());
+
+            const aString = a.toString(16);
+
+            const wrapperingModulus = (new BN(1)).ushln(modulusLength * 8);
+            let result;
+            if (a.cmp(b) == -1) {
+                result = wrapperingModulus.sub(b.sub(a))
+            } else {
+                result = a.sub(b)
+            }
+
+            const aLimbs = bnToAccumulator(a, NlengthIn32ByteLimbs.toNumber())
+
+            const bLimbs = bnToAccumulator(b, NlengthIn32ByteLimbs.toNumber())
+            const contractResultLimbs = await contract.wrappingSub(aLimbs, bLimbs);
+
+            const resultString = result.toString(16);
+            let resultLimbsString = contractResultLimbs[0].toString(16)
+            for (let i = 1; i < NlengthIn32ByteLimbs.toNumber(); i++) {
+                resultLimbsString = resultLimbsString + contractResultLimbs[i].toString(16).padStart(64, "0");
+            }
+            assert(resultString === resultLimbsString, "Invalid conversion to limbs");
+
+            const contractResult = accumulatorToBN(contractResultLimbs, NlengthIn32ByteLimbs.toNumber());
+
+            assert(contractResult.eq(result), "Subtraction results are different");
+        }
+    })
+
+    it('test comparison', async () => {
+        for (let j = 0; j < 10; j++) {
+            const NlengthIn32ByteLimbs = await contract.NlengthIn32ByteLimbs();
+            const modulusLength = NlengthIn32ByteLimbs.toNumber() * 32
+            const a = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const b = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            
+            const result = a.cmp(b)
+
+            const aLimbs = bnToAccumulator(a, NlengthIn32ByteLimbs.toNumber())
+            
+            const bLimbs = bnToAccumulator(b, NlengthIn32ByteLimbs.toNumber())
+
+            const contractResult = await contract.compare(aLimbs, bLimbs);
+
+            assert(result === contractResult.toNumber(), "Comparison results are different");
+        }
+        const NlengthIn32ByteLimbs = await contract.NlengthIn32ByteLimbs();
+        const modulusLength = NlengthIn32ByteLimbs.toNumber() * 32
+        const a = new BN(crypto.randomBytes(modulusLength), "16", "be");
+        const b = a.clone()
+
+        const aLimbs = bnToAccumulator(a, NlengthIn32ByteLimbs.toNumber())
+        
+        const bLimbs = bnToAccumulator(b, NlengthIn32ByteLimbs.toNumber())
+
+        const contractResult = await contract.compare(aLimbs, bLimbs);
+
+        assert(0 === contractResult.toNumber(), "Comparison results are different");
+    })
+
+    it('test modular addition', async () => {
+        for (let j = 0; j < 10; j++) {
+            const NlengthIn32ByteLimbs = await contract.NlengthIn32ByteLimbs();
+            const modulusLength = NlengthIn32ByteLimbs.toNumber() * 32
+            const a = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const b = new BN(crypto.randomBytes(modulusLength), "16", "be");
+            const modulusLimbs = await contract.getN();
+            const modulus = accumulatorToBN(modulusLimbs, NlengthIn32ByteLimbs.toNumber());
+            const comparison = a.add(b).cmp(modulus);
+            const result = a.add(b).umod(modulus);
+
+            const aLimbs = bnToAccumulator(a, NlengthIn32ByteLimbs.toNumber())
+            
+            const bLimbs = bnToAccumulator(b, NlengthIn32ByteLimbs.toNumber())
+            const contractResultLimbs = await contract.modularAdd(aLimbs, bLimbs, modulusLimbs);
+
+            const resultString = result.toString(16);
+            let resultLimbsString = contractResultLimbs[0].toString(16)
+            for (let i = 1; i < NlengthIn32ByteLimbs.toNumber(); i++) {
+                resultLimbsString = resultLimbsString + contractResultLimbs[i].toString(16).padStart(64, "0");
+            }
+            assert(resultString === resultLimbsString, "Invalid conversion to limbs for iteration " + j + " and comparison " + comparison);
+
+            const contractResult = accumulatorToBN(contractResultLimbs, NlengthIn32ByteLimbs.toNumber());
+
+            assert(contractResult.eq(result), "Addition results are different for iteration " + j);
+        }
+    })
+
+    it('make proof and check proof', async () => {
+        const coinID = 1;
+        const x = 1; // there will be no other included coins
+
+        const NlengthIn32ByteLimbs = await contract.NlengthIn32ByteLimbs();
+        const initialAccumulator = await contract.accumulator(NlengthIn32ByteLimbs.toNumber() - 1)
+        assert(initialAccumulator.toNumber() === 3);
+
+        const coinMapping = await contract.mapCoinToPrime(coinID);
+        const submissionResult = await contract.includeCoin(coinID);
+        const updatedAccumulator = await contract.accumulator(NlengthIn32ByteLimbs.toNumber() - 1)
+
+        // this is a trivial check, for a last limb only
+        const expectedAccumulator = initialAccumulator.pow(coinMapping);
+        assert(expectedAccumulator.eq(updatedAccumulator));
+
+        const proof = await contract.calculateProof(coinID, x);
+
+        const isValid = await contract.checkProof(coinID, proof[0], proof[1], proof[2]);
+    
+        assert(isValid, "Proof is invalid");
+    })
+
+    function accumulatorToBN(accumulator, numLimbs) {
+        const shift = 256;
+        let newBN = (new BN(accumulator[0].toString(16), 16))
+        for (let i = 1; i < numLimbs; i++) {
+            newBN.iushln(shift);
+            const limb = (new BN(accumulator[i].toString(16), 16))
+            newBN.iadd(limb);
+        }
+        return newBN;
+    }
+
+    function bnToAccumulator(bn, numLimbs) {
+        const newBN = bn.clone()
+        const reversedAccumulator = [];
+        const shift = 256;
+        const modulus = (new BN(1).iushln(256));
+        let limb = newBN.umod(modulus);
+        reversedAccumulator.push(limb);
+        for (let i = 1; i < numLimbs; i++) {
+            newBN.iushrn(shift);
+            limb = newBN.umod(modulus);
+            reversedAccumulator.push(limb);
+        }
+        return reversedAccumulator.reverse();
+    }
+
+    function findFirstDiffPos(a, b) {
+        if (a.length < b.length) [a, b] = [b, a];
+        return [...a].findIndex((chr, i) => chr !== b[i]);
+      }
+});
